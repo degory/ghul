8ddfbc695d4a6ef97290de2ef4b860fc18ad1918
degory <jeek@giantblob.com>
2017-09-02 11:09:32 +0100

    Force docker pull on image builds

--- docker/build.sh
+++ docker/build.sh
@@ -7,13 +7,13 @@ fi
 if [ -z "$BUILD_NUMBER" ]; then
     echo "Building test version of $1..."
     
-    docker build $1 -t docker.giantblob.com/$1:test -t docker.giantblob.com/$1:latest
+    docker build --pull $1 -t docker.giantblob.com/$1:test -t docker.giantblob.com/$1:latest
     docker push docker.giantblob.com/$1:test
     docker push docker.giantblob.com/$1:latest    
 else
     echo "Building version $BUILD_NUMBER of $1..."
     
-    docker build $1 -t docker.giantblob.com/$1:$BUILD_NUMBER -t docker.giantblob.com/$1:latest
+    docker build --pull $1 -t docker.giantblob.com/$1:$BUILD_NUMBER -t docker.giantblob.com/$1:latest
     docker push docker.giantblob.com/$1:$BUILD_NUMBER
     docker push docker.giantblob.com/$1:latest   
 fi
