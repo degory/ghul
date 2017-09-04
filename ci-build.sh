8fa54a7d2b1bc65f4db934c2929b1587f1e019c0
degory <jeek@giantblob.com>
2017-09-04 23:11:24 +0100

    Docker pull ghul compiler image before CI build

--- ci-build.sh
+++ ci-build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+pushd docker
+./pull-all.sh
+popd
 docker run -v /var/lib/jenkins/workspace/:/var/lib/jenkins/workspace/ -w $WORKSPACE --user jenkins -t docker.giantblob.com/ghul-ci /bin/bash -c "./clean.sh && ./build.sh"
 
