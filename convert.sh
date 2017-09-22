b3399618ea278279130de8c6414c8e6476b1f2c4
degory <degory@dev.giantblob.com>
2017-09-22 15:18:30 +0200

    Remove old scripts

--- convert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir /tmp/lcache
-find driver ioc system logging source lexical syntax -name '*.l' | xargs ./lc -X imports.l
-rm source/*.ghul
--- ghul.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -v `pwd`:/home/dev/source/ -w /home/dev/source --user dev -it docker.giantblob.com/ghul /bin/bash
