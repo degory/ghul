9351883b1d88c9da463000d7b224ad7b623a6d15
degory <jeek@giantblob.com>
2017-10-29 15:17:18 +0000

    bb-bootstrap.sh edited online with Bitbucket

--- bb-bootstrap.sh
+++ bb-bootstrap.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-echo "namespace Source is class BUILD is public static System.String number=\"bb\"; si si" >source/build.l
 echo "Bootstrap pass 1..."
+echo "namespace Source is class BUILD is public static System.String number=\"bb-1\"; si si" >source/build.l
 export GHUL=/usr/bin/ghul
 ./clean.sh
 ./build.sh && \
 ./test.sh || \
 exit 1
 echo "Bootstrap pass 2..."
+echo "namespace Source is class BUILD is public static System.String number=\"bb-2\"; si si" >source/build.l
 export GHUL=`pwd`/ghul
 ./clean.sh
 ./build.sh && \
 ./test.sh || \
 exit 1
 echo "Bootstrap pass 3..."
+echo "namespace Source is class BUILD is public static System.String number=\"bb-bs\"; si si" >source/build.l
 ./clean.sh
 ./build.sh && \
 ./test.sh || \
