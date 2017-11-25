e99fa69495596c806942901e921c1ad97f5868ed
degory <degs@giantblob.com>
2017-11-25 09:42:08 +0000

    CircleCI workflow

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
+echo pull
 docker pull ghul/compiler:release-candidate
+echo tag
 docker tag ghul/compiler:release-candidate ghul/compiler:stable
+echo push
 docker push ghul/compiler:stable
+echo done
 
 
