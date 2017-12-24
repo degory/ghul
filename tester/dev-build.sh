3b5dafd41f595ab24ab877c824fa6887b48ca97b
degory <degs@giantblob.com>
2017-12-24 23:34:00 +0000

    WIP poison syntax trees

---
+++ tester/dev-build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+LCACHE=lcache-test
+docker volume create $LCACHE
+
+docker run --rm -e LFLAGS="-FB -FN" -v ${LCACHE}:/tmp/lcache/ -v `pwd`:/home/dev/source/ -w /home/dev/source -u `id -u`:`id -g` -t ghul/compiler:stable ./build.sh
