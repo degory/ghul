f38c7727edd2418b3039636cb4372e3463703143
degory <degs@giantblob.com>
2018-01-03 20:29:51 +0000

    Clean ups

---
+++ profile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "namespace Source is class BUILD is public static System.String number=\"local-`date +'%s'`\"; si si" >source/build.l
+
+if [ -z "$GHUL" ]; then
+    export PATH=$PATH:`pwd`
+    export GHUL=`which ghul`
+fi
+
+export LFLAGS="-Ws -WM -FB -FN"
+
+echo "Profiling $GHUL (`$GHUL`)..."
+find lib compiler driver ioc system logging source lexical syntax semantic llvm ir -name '*.ghul' | xargs valgrind --tool=callgrind $GHUL -D -P ghul -G -X -o ghul imports.l source/*.l
