db5f5d148cb88d0194dca823ccb315cde9ea9446
degory <degs@giantblob.com>
2018-01-06 23:03:52 +0000

    Name builds for branch

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,12 +14,18 @@ if [ -z "$BUILD_NUMBER" ]; then
     BUILD_NUMBER="ad-hoc-`date +'%s'`"
 fi
 
+if [ -z "$BRANCH_NAME" ]; then
+    BRANCH_NAME=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
+fi
+
 echo $BUILD_NUMBER: Starting bootstrap...
 
-for PASS in "${BUILD_NUMBER}-1" "${BUILD_NUMBER}-2" "${BUILD_NUMBER}" ; do
+BUILD_NAME="${BUILD_NUMBER}-${BRANCH_NAME}"
+
+for PASS in "${BUILD_NAME}-1" "${BUILD_NAME}-2" "${BUILD_NAME}" ; do
     echo $PASS: Start compile...
 
-    echo "namespace Source is class BUILD is public static System.String number=\"$PASS\"; si si" >source/build.l
+    echo "namespace Source is class BUILD is public static System.String number=\"${PASS}\"; si si" >source/build.l
 
     docker run --name "bootstrap-`date +'%s'`" --rm -e LFLAGS="$LFLAGS" -e GHUL=/usr/bin/ghul -v `pwd`:/home/dev/source -w /home/dev/source -u `id -u`:`id -g` $BUILD_WITH bash -c ./build.sh || exit 1
 
@@ -33,7 +39,7 @@ for PASS in "${BUILD_NUMBER}-1" "${BUILD_NUMBER}-2" "${BUILD_NUMBER}" ; do
 
     echo $PASS: Image built
 
-    if [ "$PASS" == "${BUILD_NUMBER}" ]; then
+    if [ "$PASS" == "${BUILD_NAME}" ]; then
         pushd tester
 
         echo $PASS: Build tester...
@@ -52,18 +58,18 @@ done
 
 echo $BUILD_NUMBER: Bootstrap complete, pushing release candidate docker images...
 
-docker tag ghul:$PASS ghul/compiler:release-candidate || exit 1
-docker push ghul/compiler:release-candidate || exit 1
+docker tag ghul:$PASS ghul/compiler:${BRANCH_NAME} || exit 1
+docker push ghul/compiler:${BRANCH_NAME} || exit 1
 
 if [ ! -z $DOCKER_REGISTRY_2 ]; then
-    docker tag ghul:$PASS ${DOCKER_REGISTRY_2}/compiler:release-candidate || exit 1
-    docker push ${DOCKER_REGISTRY_2}/compiler:release-candidate || exit 1
+    docker tag ghul:$PASS ${DOCKER_REGISTRY_2}/compiler:${BRANCH_NAME} || exit 1
+    docker push ${DOCKER_REGISTRY_2}/compiler:${BRANCH_NAME} || exit 1
 fi
 
-docker tag ghul:$PASS ghul/compiler:${BUILD_NUMBER} || exit 1
-docker push ghul/compiler:${BUILD_NUMBER} || exit 1
+docker tag ghul:$PASS ghul/compiler:${BRANCH_NAME}-${BUILD_NUMBER} || exit 1
+docker push ghul/compiler:${BRANCH_NAME}-${BUILD_NUMBER} || exit 1
 
 if [ ! -z $DOCKER_REGISTRY_2 ]; then
-    docker tag ghul:$PASS ${DOCKER_REGISTRY_2}/compiler:${BUILD_NUMBER} || exit 1
-    docker push ${DOCKER_REGISTRY_2}/compiler:${BUILD_NUMBER} || exit 1
+    docker tag ghul:$PASS ${DOCKER_REGISTRY_2}/compiler:${BRANCH_NAME}-${BUILD_NUMBER} || exit 1
+    docker push ${DOCKER_REGISTRY_2}/compiler:${BRANCH_NAME}-${BUILD_NUMBER} || exit 1
 fi
--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
+
+if [ -z "$BRANCH_NAME" ]; then
+    BRANCH_NAME=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
+fi
+
 echo pull
-docker pull ghul/compiler:release-candidate
+docker pull ghul/compiler:${BRANCH_NAME}
 echo tag
-docker tag ghul/compiler:release-candidate ghul/compiler:stable
+docker tag ghul/compiler:${BRANCH_NAME} ghul/compiler:stable
 echo push
 docker push ghul/compiler:stable
 
 if [ ! -z $DOCKER_REGISTRY_2 ]; then
     echo tag
-    docker tag ghul/compiler:release-candidate ${DOCKER_REGISTRY_2}/compiler:stable || exit 1
+    docker tag ghul/compiler:${BRANCH_NAME} ${DOCKER_REGISTRY_2}/compiler:stable || exit 1
     echo push
     docker push ${DOCKER_REGISTRY_2}/compiler:stable || exit 1
 fi
