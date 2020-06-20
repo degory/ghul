46cc8ede3146a0d8572f8580fc70badb8b91f9ab
degory <degory@users.noreply.github.com>
2020-06-20 23:07:08 +0100

    Use build name as Docker tag rather than branch name

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 set -e
 
+if [ ! -z '$GITHUB_RUN_ID' ]; then
+    BUILD_NUMBER=$GITHUB_RUN_ID
+fi
+
+if [ -z "$BUILD_NUMBER" ]; then
+    BUILD_NUMBER="ad-hoc-`date +'%s'`"
+fi
+
 if [ -z "$BRANCH_NAME" ]; then
     BRANCH_NAME=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
 fi
 
+BUILD_NAME="${BRANCH_NAME}-${BUILD_NUMBER}"
+
 echo pull
-docker pull ghul/compiler:${BRANCH_NAME}
+docker pull ghul/compiler:${BUILD_NAME}
 echo tag
-docker tag ghul/compiler:${BRANCH_NAME} ghul/compiler:stable
+docker tag ghul/compiler:${BUILD_NAME} ghul/compiler:stable
 echo push
 docker push ghul/compiler:stable
 
 if [ ! -z $DOCKER_REGISTRY_2 ]; then
     echo tag
-    docker tag ghul/compiler:${BRANCH_NAME} ${DOCKER_REGISTRY_2}/compiler:stable || exit 1
+    docker tag ghul/compiler:${BUILD_NAME} ${DOCKER_REGISTRY_2}/compiler:stable || exit 1
     echo push
     docker push ${DOCKER_REGISTRY_2}/compiler:stable || exit 1
 fi
