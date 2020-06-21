8359cdbae5e130ebb4a560def0332478b30ebfcc
degory <degs@giantblob.com>
2020-06-21 19:54:17 +0100

    Remove references to LLVM

--- build-llvm-stub.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-gcc -fPIC -shared llvm-5.0-stub.c -o libLLVM-5.0.so.1
-gcc -fPIC -shared llvm-6.0-stub.c -o libLLVM-6.0.so.1
-gcc -fPIC -shared llvm-7.0-stub.c -o libLLVM-7.0.so.1
-gcc -fPIC -shared llvm-8.0-stub.c -o libLLVM-8.0.so.1
