999ea3b4436d95e80cedd2e799fe00d67edadcf9
degory <degs@giantblob.com>
2020-06-28 11:30:14 +0100

    Fix tests. Simplify test run script. Delete old test run scripts.

--- dev-test.sh
+++ dev-test.sh
@@ -1,34 +1,3 @@
 #!/bin/bash
+docker run --name "test-`date +'%s'`" --rm --env PATH='/home/dev/source:/bin:/usr/bin:/usr/local/bin' -v test-lcache:/tmp/lcache -v `pwd`:/home/dev/source/ -w /home/dev/source/test -u `id -u`:`id -g` ghul/compiler:stable ../tester/tester ${@:1}
 
-ARG=$1
-
-if [[ $ARG =~ .ghul$ ]] ; then
-    CASE=`dirname $ARG`
-    NAME=`basename $CASE`
-    PARENT=`dirname $CASE`
-
-    if [[ $PARENT =~ test/cases$ ]] ; then
-        echo "Run test containing $ARG, test case $NAME"
-
-        TEST=$NAME
-    else
-        echo "Run all tests"
-    fi
-elif [ -z $ARG ] ; then
-    echo "Run all test cases"
-elif [[ -d test/cases/$ARG ]] ; then
-    echo "Run test case $ARG"
-else
-    CASE=`echo test/cases/$ARG*`
-
-    if [ -d $CASE ] ; then
-        TEST=`basename $CASE`
-        echo "Run test case $TEST"
-    else
-        echo "Not a test case or a test source file $ARG, running all tests"
-    fi
-fi
-
-docker run --name "test-`date +'%s'`" --rm --env PATH='/home/dev/source:/bin:/usr/bin:/usr/local/bin' -v test-lcache:/tmp/lcache -v `pwd`:/home/dev/source/ -w /home/dev/source/test -u `id -u`:`id -g` ghul/compiler:stable ../tester/tester $TEST
-
-cat test/junit.xml
