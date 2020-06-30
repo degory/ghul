6d6ce3eeae1a8cb8a3c33c291012e273b5901ec3
degory <degory@users.noreply.github.com>
2020-06-30 22:28:15 +0100

    Migrate version number file from L to Ghul (#120)

--- dev-build.sh
+++ dev-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "namespace Source is class BUILD is public static System.String number=\"local-`date +'%s'`\"; si si" >source/build.l
+echo "namespace Source is class BUILD is number: System.String static => \"local-`date +'%s'`\"; si si" >source/build.ghul
 
 if [ -z "$GHUL" ]; then
     export PATH=$PATH:`pwd`
@@ -10,4 +10,4 @@ fi
 export LFLAGS="-Ws -WM -FC"
 
 echo "Building with $GHUL (`$GHUL`)..."
-find compiler driver ioc system logging source lexical syntax semantic ir -name '*.ghul' |  xargs $GHUL -D -P ghul -L $GHULFLAGS -o ghul imports.l source/*.l
+find compiler driver ioc system logging source lexical syntax semantic ir -name '*.ghul' |  xargs $GHUL -D -P ghul -L $GHULFLAGS -o ghul imports.l 
--- profile.sh
+++ profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "namespace Source is class BUILD is public static System.String number=\"local-`date +'%s'`\"; si si" >source/build.l
+echo "namespace Source is class BUILD is number: System.String static => \"local-`date +'%s'`\"; si si" >source/build.ghul
 
 if [ -z "$GHUL" ]; then
     export PATH=$PATH:`pwd`
@@ -10,4 +10,4 @@ fi
 export LFLAGS="-Ws -WM -FB -FN"
 
 echo "Profiling $GHUL (`$GHUL`)..."
-find lib compiler driver ioc system logging source lexical syntax semantic ir -name '*.ghul' | xargs valgrind --tool=callgrind $GHUL -D -P ghul -G -X -o ghul imports.l source/*.l
+find lib compiler driver ioc system logging source lexical syntax semantic ir -name '*.ghul' | xargs valgrind --tool=callgrind $GHUL -D -P ghul -G -X -o ghul imports.l
