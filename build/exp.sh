2bc76fe8619cb7782bbce6f3ec1796cf5c25a76f
degory <degory@users.noreply.github.com>
2020-07-10 19:21:29 +0200

    Type check assignments (#160)

--- build/exp.sh
+++ build/exp.sh
@@ -2,4 +2,4 @@
 
 echo "namespace Source is class BUILD is number: System.String static => \"local-`date +'%s'`\"; si si" >src/source/build.ghul
 
-find compiler driver ioc system logging source lexical syntax semantic ir lib -name '*.ghul' |  xargs ./ghul -G -X $GHULFLAGS 
+find src lib -name '*.ghul' |  xargs ./ghul -G -X $GHULFLAGS 
