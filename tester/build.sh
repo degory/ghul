53300603f437c4129c5b15ff731445e1a1e90933
degory <degory@users.noreply.github.com>
2020-07-15 19:18:03 +0200

    Fix issue where bootstrap failed on WSL + Docker (#172)

--- tester/build.sh
+++ tester/build.sh
@@ -3,4 +3,4 @@
 LCACHE=lcache-test
 docker volume create $LCACHE
 
-docker run --name "dev-`date +'%s'`" --rm -e LFLAGS="-FB -FN -Ws -WM" -v ${LCACHE}:/tmp/lcache/ -v `pwd`:/home/dev/source/ -w /home/dev/source -u `id -u`:`id -g` ghul/compiler:stable ./_build.sh
+docker run --name "dev-`date +'%s'`" --rm -e LFLAGS="-FB -FN -Ws -WM" -v ${LCACHE}:/tmp/lcache/ -v `pwd`:/home/dev/source/ -w /home/dev/source -u `id -u`:`id -g` ghul/compiler:stable /bin/sh ./_build.sh
