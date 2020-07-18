7bddb2e019102a774c26382b05f4b6a23ce16fcb
quanglewangle <peter@quanglewangle.com>
2020-07-18 12:29:00 +0100

    Anonymous volume in build.sh (#180)
    * build script  includes the user's UID in the volume name
    
    * Use docker anonymous volume. removed docker volume create line and 'name:'  part of docker run -v flag
    
    * deleted redundant file
    
    Co-authored-by: peter <peter@code.giantblob.com>

--- tester/build.sh
+++ tester/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-LCACHE=lcache-test
-docker volume create $LCACHE
 
-docker run --name "dev-`date +'%s'`" --rm -e LFLAGS="-FB -FN -Ws -WM" -v ${LCACHE}:/tmp/lcache/ -v `pwd`:/home/dev/source/ -w /home/dev/source -u `id -u`:`id -g` ghul/compiler:stable /bin/sh ./_build.sh
+docker run --name "dev-`date +'%s'`" --rm -e LFLAGS="-FB -FN -Ws -WM" -v /tmp/lcache/ -v `pwd`:/home/dev/source/ -w /home/dev/source -u `id -u`:`id -g` ghul/compiler:stable /bin/sh ./_build.sh
