6da4cc4ea3a3405f88cee1aa3407f1a022b33b05
degory <degs@giantblob.com>
2020-07-23 22:31:10 +0100

    Work on IL for static members and properties

--- build/install.sh
+++ build/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -v /tmp:/home/dev/source/ -w /home/dev/source -u `id -u`:`id -g` ghul/compiler:stable /bin/bash -c "cp /usr/bin/ghul /usr/lib/ghul/ghul.ghul /home/dev/source"
+docker run --rm -v /tmp:/home/dev/source/ -w /home/dev/source -u `id -u`:`id -g` ghul/compiler:stable /bin/sh -c "cp /usr/bin/ghul /home/dev/source; cp -r /usr/lib/ghul /home/dev/src/lib"
 sudo -- sh -c "mv /tmp/ghul /usr/bin ; chown root:root /usr/bin/ghul ; mkdir -p /usb/lib/ghul ; mv /tmp/ghul.ghul /usb/lib/ghul ; chown root:root /usr/lib/ghul/ghul.ghul"
