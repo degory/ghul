c459463c3813b903fdcea4c5531b81015234d01a
degory <degory@users.noreply.github.com>
2020-08-03 10:12:20 +0100

    Pull stable container before install (#232)

--- build/install.sh
+++ build/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo -- sh -c "rm -r /tmp/ghul /tmp/lib" 2>/dev/null
+docker pull ghul/compiler:stable
 docker run --rm -v /tmp:/home/dev/source/ -w /home/dev/source -u `id -u`:`id -g` ghul/compiler:stable /bin/bash -c "cp /usr/bin/ghul /home/dev/source; cp -r /usr/lib/ghul /home/dev/source/lib"
 sudo -- sh -c "mv /tmp/ghul /usr/bin ; chown root:root /usr/bin/ghul ; mkdir -p /usr/lib/ghul ; rm -r /usr/lib/ghul/* 2>/dev/null ; cp -r /tmp/lib/* /usr/lib/ghul/ ; chown -R root:root /usr/lib/ghul/ ; rm -r /tmp/lib"
+/usr/bin/ghul
