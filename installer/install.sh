7c24a9d7f4fc602f3cd8387591a092bd4c1e239b
degory <degory@users.noreply.github.com>
2020-09-10 21:30:13 +0200

    Check prerequisites before installing (#318)

--- installer/install.sh
+++ installer/install.sh
@@ -1,1 +1,50 @@
-sudo cp -rv usr /tmp
+#!/bin/bash
+
+if ldconfig -p | fgrep -q libgc.so.1 ; then echo
+    echo "✅ libgc found"
+else
+    echo "❌ libgc.so.1 not found: please install libgc1c2 (e.g. apt install libgc1c2)"
+    FAILED=1
+fi
+
+if [ -x "`which ilasm`" ] ; then
+    echo "✅ ilasm found"
+else
+    echo "❌ ilasm not found: please install mono SDK (https://www.mono-project.com/download/stable/)"
+    FAILED=1
+fi
+
+if [ $FAILED ]; then
+    echo "❌ prerequisites check failed: please correct and retry"
+    exit 1
+fi
+
+if [ `id -u` == 0 ] ; then
+    echo "✅ you are root: sudo not required"
+    PREFIX="";
+
+elif [ -x "`which sudo`" ] ; then
+    echo "✅ you are not root, but sudo found: please enter your password if prompted"
+    PREFIX="sudo"
+else
+    echo "❌ you are not root and sudo not found on the path: giving up"
+    exit 1
+fi
+
+if [ -d /usr/lib/ghul ] ; then
+    if $PREFIX rm -r /usr/lib/ghul /usr/bin/ghul ; then
+        echo "✅ existing ghūl installation removed"
+    else
+        echo "❌ failed to remove existing ghūl installation: please manually delete /usr/lib/ghul/"
+        exit 1
+    fi
+fi
+
+if $PREFIX cp -r usr / ; then
+    echo "✅ ghūl compiler installed"
+else
+    echo "❌ failed to copy files"
+    exit 1
+fi
+
+/usr/bin/ghul
