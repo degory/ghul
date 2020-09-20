dfc9420b47add06e85c0483d6d4a5e666f95095e
degory <degs@giantblob.com>
2020-09-20 14:29:41 +0100

    Fix premature exit from bootstrap script

--- build/set-build-name.sh
+++ build/set-build-name.sh
@@ -1,41 +1,38 @@
-echo BUILD_NAME initially: \"${BUILD_NAME}\"
-
 if [ ! -z "${BUILD_NAME}" ] ; then
     echo BUILD_NAME overridden to \"${BUILD_NAME}\"
-    exit
-fi
+else
 
-if [ ! -z "${GITHUB_RUN_ID}" ]; then
-    BUILD_NUMBER=${GITHUB_RUN_ID}
-fi
-
-if [ -z "${BUILD_NUMBER}" ]; then
-    BUILD_NUMBER="`date +'%s'`"
-fi
-
-if [ -z "${BRANCH_NAME}" ]; then
-    BRANCH_NAME=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
-fi
-
-if [ ${CI} ] ; then
-    if [ "${BRANCH_NAME}" == "master" ] ; then
-        BRANCH_NAME="stable"
+    if [ ! -z "${GITHUB_RUN_ID}" ]; then
+        BUILD_NUMBER=${GITHUB_RUN_ID}
     fi
-fi
 
-if [ -z "${ENVIRONMENT}" ] ; then
-    if [ ${GITHUB_ACTIONS} ] ; then
-        ENVIRONMENT="github"
-    elif [ ${CIRCLECI} ] ; then
-        ENVIRONMENT="circleci"
-    elif [ ${CI} ] ; then
-        ENVIRONMENT="ci"
-    else
-        ENVIRONMENT="local"
+    if [ -z "${BUILD_NUMBER}" ]; then
+        BUILD_NUMBER="`date +'%s'`"
     fi
+
+    if [ -z "${BRANCH_NAME}" ]; then
+        BRANCH_NAME=`git branch | sed -n -e 's/^\* \(.*\)/\1/p'`
+    fi
+
+    if [ ${CI} ] ; then
+        if [ "${BRANCH_NAME}" == "master" ] ; then
+            BRANCH_NAME="stable"
+        fi
+    fi
+
+    if [ -z "${ENVIRONMENT}" ] ; then
+        if [ ${GITHUB_ACTIONS} ] ; then
+            ENVIRONMENT="github"
+        elif [ ${CIRCLECI} ] ; then
+            ENVIRONMENT="circleci"
+        elif [ ${CI} ] ; then
+            ENVIRONMENT="ci"
+        else
+            ENVIRONMENT="local"
+        fi
+    fi
+
+    BUILD_NAME="${BRANCH_NAME}-${ENVIRONMENT}-${BUILD_NUMBER}"
+
+    echo BUILD_NAME set to "${BUILD_NAME}"
 fi
-
-BUILD_NAME="${BRANCH_NAME}-${ENVIRONMENT}-${BUILD_NUMBER}"
-
-echo BUILD_NAME set to "${BUILD_NAME}"
-
