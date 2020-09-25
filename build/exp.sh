176548fc949bca4c4b5427f62677425fca732eae
degory <degs@giantblob.com>
2020-09-25 19:57:29 +0100

    WIP

--- build/exp.sh
+++ build/exp.sh
@@ -2,11 +2,4 @@
 
 echo "namespace Source is class BUILD is number: System.String static => \"local-`date +'%s'`\"; si si" >src/source/build.ghul
 
-docker run --name "typecheck-`date +'%s'`" --rm \
-    -v `pwd`:/usr/local/bin \
-    -v `pwd`:/home/dev/source/ \
-    -v `pwd`/lib:/usr/lib/ghul \
-    -w /home/dev/source/ \
-    -u `id -u`:`id -g` \
-    ghul/mono-base \
-    bash -c "find src -name '*.ghul' | xargs ./ghul -G -X ./lib/ghul.ghul" 
+find src -name '*.ghul' | xargs ./ghul -G -X -p ./lib -l legacy/ghul
