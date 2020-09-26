a9a67719d47d1795675d9f24d6fb59f26eda9e70
degory <degory@users.noreply.github.com>
2020-09-26 16:46:49 +0100

    Bug fixes (#357)
    - Fix issue with code completion where only very limited set of completions was offered, caused by an issue in the sorted map collection
    - Fix garbled symbol descriptions resulting from altered collection class behaviour
    - Fix some oopses triggered by incomplete source code
    - Fix oops caused by reference to generic type argument ancestors
    - Remove test run steps from bootstrap script
    - Push bootstrapped container
    - Run legacy tests workflow job using bootstrapped container instead of release candidate container
    - Add conditions to the tag container and create release steps, to allow the release workflow to be tested from a feature branch

--- build/bootstrap.sh
+++ build/bootstrap.sh
@@ -14,7 +14,7 @@ source ./build/set-build-name.sh
 
 echo $BUILD_NAME: Starting bootstrap...
 
-for PASS in "${BUILD_NAME}-bs-1" "${BUILD_NAME}-bs-2" ; do
+for PASS in "ghul/bootstrap:${BUILD_NAME}-1" "ghul/bootstrap:${BUILD_NAME}" ; do
     echo $PASS: Start compile...
 
     echo "namespace Source is class BUILD is number: System.String public static => \"${PASS}\"; si si" >src/source/build.ghul
@@ -29,27 +29,9 @@ for PASS in "${BUILD_NAME}-bs-1" "${BUILD_NAME}-bs-2" ; do
 
     echo $PASS: Build image...
 
-    BUILD_WITH=ghul:$PASS
+    BUILD_WITH=$PASS
 
     docker build --pull -t $BUILD_WITH . || exit 1
 
     echo $PASS: Image built
-
-    if [ "$PASS" == "${BUILD_NAME}-bs-2" ]; then
-        pushd tester
-
-        echo $PASS: Build tester...
-
-        ./build.sh
-
-        popd
-
-        echo $PASS: Start tests... 
-
-        docker run --name "bootstrap-`date +'%s'`" --rm -v `pwd`:/home/dev/source/ -w /home/dev/source/tests-legacy -u `id -u`:`id -g` $BUILD_WITH ../tester/tester
-
-        docker run --name "bootstrap-`date +'%s'`" --rm -v `pwd`:/usr/local/bin -v `pwd`:/home/dev/source/ -v `pwd`/lib:/usr/lib/ghul -w /home/dev/source/tests -u `id -u`:`id -g` ghul/mono-base ../tester/tester
-
-        echo $PASS: Tests complete
-    fi
 done
