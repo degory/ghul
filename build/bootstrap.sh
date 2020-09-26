eb36e62ca56c6409a3ddc316182fca4d47c8caf9
degory <degs@giantblob.com>
2020-09-26 15:39:25 +0100

    Do not run test suites twice for release workflow - Remove test run steps from bootstrap script - Run legacy tests workflow job using bootstrapped container instead of release candidate container - Add conditions to the tag container and create release steps, to allow the release workflow to be tested from a feature branch

--- build/bootstrap.sh
+++ build/bootstrap.sh
@@ -34,22 +34,4 @@ for PASS in "${BUILD_NAME}-bs-1" "${BUILD_NAME}-bs-2" ; do
     docker build --pull -t $BUILD_WITH . || exit 1
 
     echo $PASS: Image built
-
-    if [ "$PASS" == "${BUILD_NAME}-bs-2" ]; then
-        pushd tester
-
-        echo $PASS: Build tester...
-
-        ./build.sh
-
-        popd
-
-        echo $PASS: Start tests... 
-
-        docker run --name "bootstrap-`date +'%s'`" --rm -v `pwd`:/home/dev/source/ -w /home/dev/source/tests-legacy -u `id -u`:`id -g` $BUILD_WITH ../tester/tester
-
-        docker run --name "bootstrap-`date +'%s'`" --rm -v `pwd`:/usr/local/bin -v `pwd`:/home/dev/source/ -v `pwd`/lib:/usr/lib/ghul -w /home/dev/source/tests -u `id -u`:`id -g` ghul/mono-base ../tester/tester
-
-        echo $PASS: Tests complete
-    fi
 done
