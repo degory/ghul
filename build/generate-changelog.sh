b2f77338fba77df1588427e5313025374d3dbb02
degory <degory@users.noreply.github.com>
2020-10-05 23:43:01 +0100

    Fix issues with anonymous functions (#384)
    - Fix anon functions returning void mapped to incorrect .NET types
    - Fix incorrect IL generated for anon functions returning void
    - Fix issue where anon functions within static methods still attempted to capture 'this' in generated IL
    - Add test cases for anon functions in static methods and anon functions returning void
    - Remove non-applicable '$ghul' problem matcher from tests' ghul configs
    - Another attempt at getting the changelog generator to behave

--- build/generate-changelog.sh
+++ build/generate-changelog.sh
@@ -2,7 +2,7 @@
 
 # not sure if I'm using it wrong or it's simply flaky but this seems to fail randomly from time to time. Try it a few times until it produces output, or we get sick of trying:
 for I in 1 2 3 ; do
-    if docker run --env CHANGELOG_GITHUB_TOKEN --rm -v `pwd`:/usr/local/src/your-app ferrarimarco/github-changelog-generator -u degory -p ghul --future-release $VERSION ; then
+    if docker run --env CHANGELOG_GITHUB_TOKEN --rm -v `pwd`:/usr/local/src/your-app ferrarimarco/github-changelog-generator -u degory -p ghul ; then
         break;
     fi
     sleep 5
