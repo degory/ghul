53b3c14766251981e315de6a3c03ede00a08c6ba
degory <degs@giantblob.com>
2020-11-11 16:38:09 +0000

    Full bootstrap onto .NET
    Stage 2 bootstrapped compiler can now compile itself to produce stage 3, and stage 3 passes all tests.
    
    Bug fixes:
    - Fix issue where string literals containing control characters, including newlines, generated invalid IL

---
+++ build/build-stage-3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "namespace Source is class BUILD is number: System.String static => \"local-`date +'%s'-stage-3`\"; si si" >src/source/build.ghul
+
+echo "Building with ./ghul.exe (`mono ./ghul.exe`) for .NET target..."
+find src -name '*.ghul' | xargs mono ./ghul-s2.exe -p ./lib -o ghul-s3.exe
