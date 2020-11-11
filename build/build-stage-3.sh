3cb833bd699ba124811f7c6d70fc7b5f59d6309b
degory <degs@giantblob.com>
2020-11-11 20:54:18 +0000

    Support ghūl language VSCE on .NET hosted compiler
    Bug fixes:
    - Fix analysis mode issues in the .NET hosted compiler that prevented it working with the ghūl Visual Code language support extension
    
    Technical improvements:
    - Add support to test runner for hosting compiler under test on .NET Core in addition to Mono and native Linux

--- build/build-stage-3.sh
+++ build/build-stage-3.sh
@@ -3,4 +3,4 @@
 echo "namespace Source is class BUILD is number: System.String static => \"local-`date +'%s'-stage-3`\"; si si" >src/source/build.ghul
 
 echo "Building with ./ghul.exe (`mono ./ghul.exe`) for .NET target..."
-find src -name '*.ghul' | xargs mono ./ghul-s2.exe -p ./lib -o ghul-s3.exe
+find src -name '*.ghul' | xargs dotnet ./ghul-s2.exe -p ./lib -o ghul-s3.exe
