717c121bc670b6889f86ac5f439de24d5316422b
degory <degory@users.noreply.github.com>
2020-11-12 23:13:30 +0000

    Bundle .NET hosted compiler in installer (#501)
    Enhancements:
    - Bundle the .NET hosted compiler executable in the installer
    - Clearer build success/failure messages on exit
    
    Bug fixes:
    - Fix issue where static properties in stub classes incorrectly caused an error
    - Fix missing assembly reference warning from ILAsm for 'System'
    - Fix incorrect assembly name for stack and queue types
    
    Technical improvements:
    - Add stubs for DateTime and various IO types required to migrate test runner to .NET
    - Remove dead code
    - Basic .NET bootstrap on PR build before running .NET tests (but workflows need redoing properly (see #485)

--- build/make-installer.sh
+++ build/make-installer.sh
@@ -9,6 +9,9 @@ tar xvzf ../root.tar.gz
 popd 2>/dev/null
 cp -av ../lib/dotnet ../lib/legacy root/usr/lib/ghul
 cp ../ghul root/usr/bin
+if [ -f ../ghul.exe ] ; then
+    cp ../ghul.exe root/usr/bin
+fi
 cp ../tester/tester root/usr/bin
 cp install.sh root
 bash ./makeself-2.4.2/makeself.sh root ghul.run "ghūl compiler" ./install.sh
