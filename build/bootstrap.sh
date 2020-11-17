c47845b4e470ede44ff818c6d6beef7fa04be682
degory <degs@giantblob.com>
2020-11-17 21:42:12 +0000

    Remove legacy code
    Technical improvements:
    - Remove all legacy only conditionally compiled code (closes #531)
    - Change bootstrap script so compiler has correct build number without needing another build after bootstrapping

--- build/bootstrap.sh
+++ build/bootstrap.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 set -e
+
+if [ -z "$BUILD_NAME" ] ; then
+    export BUILD_NAME="bootstrap-`date +'%s'`"
+fi
+
 ./build/build.sh
-BUILD_NAME=bootstrap GHUL=./ghul.exe ./build/build.sh
+GHUL=./ghul.exe ./build/build.sh
 mv out.il stage-2.il
-BUILD_NAME=bootstrap GHUL=./ghul.exe ./build/build.sh
+GHUL=./ghul.exe ./build/build.sh
 mv out.il stage-3.il
 diff stage-2.il stage-3.il
 rm stage-2.il stage-3.il
