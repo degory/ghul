eadc7c461710f58fe5ad37fb5b1daf749536fbbf
degory <degory@users.noreply.github.com>
2020-12-02 08:18:28 +0000

    Support passing function arguments by reference (#587)
    Support passing function arguments by reference
    
    Enhancements:
    - Add `ref` operator, which creates references to variables or expression values suitable for passing to functions expecting `ref` typed arguments (closes #582)
    
    Bug fixes:
    - Ambiguous function calls should error (closes #585)
    - Arguments in wrong order in overload error messages (closes #584)
    - Failing container tests do not fail the release build (closes #588)
    
    Technical:
    - Innate property support (closes #265)
    - Push version tagged development container images (closes #590)
    - Add stubs for additional types in System.Threading

--- tests/run-failed.sh
+++ tests/run-failed.sh
@@ -2,7 +2,7 @@
 for f in `find tests -name failed`; do 
     DIR=`dirname $f`
 
-    if [ ! -f $DIR/disabled ] ; then
-        echo `dirname $f`
+    if [ ! -f "$DIR/disabled" ] ; then
+        echo $DIR
     fi
 done | xargs ghul-test
