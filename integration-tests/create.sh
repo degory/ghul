76f4f46c502e6b74748e02f7191d501adab5a5db
degory <degory@users.noreply.github.com>
2025-06-04 22:15:03 +0200

    Allow creating test noninteractively (#1183)

--- integration-tests/create.sh
+++ integration-tests/create.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-ROOT=${1}
+ROOT="$1"
+TEST_NAME="$2"
 
 if [ -z "${ROOT}" ] ; then
-    ROOT=`pwd`
+    ROOT="`pwd`"
 fi
 
 echo will create test under "${ROOT}/integration-tests"
 
-read -p "new test name (folder/kebab-case-test-name): " TEST_NAME
+if [ -z "${TEST_NAME}" ] ; then
+    read -p "new test name (folder/kebab-case-test-name): " TEST_NAME
+fi
 
 if [ -z "${TEST_NAME}" ] ; then
     echo "no test name given"
